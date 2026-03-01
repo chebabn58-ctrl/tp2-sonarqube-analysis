796ec3336f8877d4e818de8bae09699fcc019428
chebabn58-ctrl <chebabn58@gmail.com>
2026-03-01 05:19:26 -0800

    Add UserService class with role printing methods

---
+++ UserService.java
@@ -0,0 +1,16 @@
+public class UserService {
+public void printRole(String role) {
+if(role.equals("ADMIN")) {
+System.out.println("Admin access");
+} else {
+System.out.println("User access");
+}
+}
+public void printRoleDuplicate(String role) {
+if(role.equals("ADMIN")) {
+System.out.println("Admin access");
+  } else {
+System.out.println("User access");
+}
+}
+}
